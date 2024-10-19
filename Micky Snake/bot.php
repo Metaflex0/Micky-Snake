e734158391efe51b10592bd1896b84b4fc72dbe0
Micky snake <metaflex560@gmail.com>
2024-10-19 12:46:49 -0700

    Update bot.php

--- Micky Snake/bot.php
+++ Micky Snake/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7574622124:AAGKwTjTs2I2hmgdu-TXj49FwYGKrulxSug";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
